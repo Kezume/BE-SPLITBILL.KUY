6004219fb65ddbcaf7e68d576e0570ad3fa87f71
Kezume <roihanabcd@gmail.com>
2026-05-01 21:29:19 +0700

    modify main.go, move and rename file database.go from migration to pkg/database/mysql.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"github.com/Kezume/BE-SPLITBILL.KUY/migration"
+	"github.com/Kezume/BE-SPLITBILL.KUY/pkg/database"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	migration.ConnectDB()
+	database.ConnectDB()
 
 	r := gin.Default()
 
